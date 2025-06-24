0de5e68526f6128627f93f36a7d12b7c00fbd13e
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-06-24 17:30:59 +0300

    up (#34)

--- src/stories/CSVHandler.stories.tsx
+++ src/stories/CSVHandler.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import CSVHandler from "../csv-handler/CSVHandler";
 import { useState } from "react";
 import { IRow } from "..";
